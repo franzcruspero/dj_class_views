8d18a9212aa512341f4aed744e807d063394013a
Franz Andrei M. Cruspero <franz.cruspero@gmail.com>
2020-03-02 01:25:11 +0800

    Initial commit

--- src/cbv/urls.py
+++ src/cbv/urls.py
@@ -3,12 +3,14 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     # Examples:
     url(r'^$', 'newsletter.views.home', name='home'),
     url(r'^contact/$', 'newsletter.views.contact', name='contact'),
-    url(r'^about/$', 'trydjango18.views.about', name='about'),
+    # url(r'^about/$', 'cbv.views.about', name='about'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
